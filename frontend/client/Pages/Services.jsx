c21a28302ac168fb1f9163f983546725eb3c2e73
sabrikawtar <sabrikawtar17@gmail.com>
2026-05-18 15:24:54 +0100

    save section services

--- frontend/client/Pages/Services.jsx
+++ frontend/client/Pages/Services.jsx
@@ -2,10 +2,23 @@ import React from 'react'
 
 function Services() {
   return (
-    <div>
-        <h1>Services</h1>
-      
-    </div>
+ <div>
+    <section className=' bg-sky-500 pt-20 pb-20 flex justify-center gap-20  '>
+          <div>
+                <h1 className='w-150 text-6xl font-bold  '>Nos Services Marketing Digital au Maroc</h1>
+
+          </div>
+
+          <div>
+                 <p className='w-130 pt-30 font-semibold text-white '>De la stratégie à l’exécution, de la visibilité à la conversion — Hooked Digital
+                   est votre partenaire 360° pour dominer le digital au Maroc.
+                   SEO, publicité, web, social media, branding, data.</p>
+
+          </div>
+
+
+    </section>
+</div>
   )
 }
 
