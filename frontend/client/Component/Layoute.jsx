d72265893c856347054ec18c79588dc8a42587fd
IDBRAHIMFATIHA <fatiiidbrahim2@gmail.com>
2026-06-03 15:31:18 +0100

    chatbox termine

--- frontend/client/Component/Layoute.jsx
+++ frontend/client/Component/Layoute.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from './Header'
 import Footer from './Footer'
+import ChatBox from "./ChatBox";
 
 function Layoute() {
   return (
@@ -9,6 +10,7 @@ function Layoute() {
       <Header />
         <Outlet />
       <Footer />
+       <ChatBox />
     </div>
   )
 }
