67bc73c1d93d7df6b4ea10591f727c1996177e97
sabrikawtar <sabrikawtar17@gmail.com>
2026-06-03 17:41:38 -0700

    save tableau

--- frontend/admin/src/App.jsx
+++ frontend/admin/src/App.jsx
@@ -1,18 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Pages/Login";
-import ProtectedRoute from "./Components/ProtectedRoute";
+import ProtectedRoute from "./components/ProtectedRoute";
 import Markup from "./Pages/Markup";
-
 
 function App() {
   return (
-<<<<<<< HEAD
-    <>
-
-    <Markup />
-    </>
-  )
-=======
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
@@ -27,7 +19,6 @@ function App() {
       </Routes>
     </Router>
   );
->>>>>>> 41eef6c711df4d6e90db6ae1c408f07f389a9b56
 }
 
 export default App;
